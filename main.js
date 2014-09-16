c81f87a417622dd7003548523d20b93e789270a4
Adam Weiner <adamwineguy@gmail.com>
2014-09-16 14:12:37 -0600

    cleaned up js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 //};
 
 
-//var word1 = prompt("Please enter any word. Any fucking word at all!");
-//alert("You entered" + " " + '"' + word1 + '"'+ "." + " The word you entered is " +  word1.length + " letters long." + " The third letter in your word is " + '"' + word1.charAt(2) + '"' + "." + " This is your word in lowercase: " + '"' + word1.toLowerCase() + '"' + "." + " This is your word in uppercase: " + '"' + word1.toUpperCase() + '"' + "." + " This is your word in a sentence: " + " You love to eat " + '"' + word1 + '"' + "." + " These are second to fourth letters in your word: " + '"' + word1.substring(1, 4) + '"' + "." + " Take that beeyatch!");
+// var word1 = prompt("Please enter any word. Any fucking word at all!");
+// alert("You entered" + " " + '"' + word1 + '"'+ "." + " The word you entered is " +  word1.length + " letters long." + " The third letter in your word is " + '"' + word1.charAt(2) + '"' + "." + " This is your word in lowercase: " + '"' + word1.toLowerCase() + '"' + "." + " This is your word in uppercase: " + '"' + word1.toUpperCase() + '"' + "." + " This is your word in a sentence: " + " You love to eat " + '"' + word1 + '"' + "." + " These are second to fourth letters in your word: " + '"' + word1.substring(1, 4) + '"' + "." + " Take that beeyatch!");
 
 
 var number1 = prompt("Please enter your phone number with dashes. We promise that we will DEFINITELY sell your personal information to third parties :P");
